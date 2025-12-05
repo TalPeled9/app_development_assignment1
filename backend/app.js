8b5e22e221cca0169bfd969f83f6c85484d5bc87
Ido Naor <idonaor15.do@gmail.com>
2025-12-05 15:50:23 +0000

    Added Express routes for posts and comments

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const routes = require('./routes');
+
 app.use(express.json());
+
+app.use('/api', routes);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
