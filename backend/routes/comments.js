3d028e1fd1adf7781d03046e24d91ad2b57ca6ac
Tal Peled <talp2002@cs.colman.ac.il>
2025-12-07 19:32:09 +0200

    Add CRUD routes for comments in comments.js

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const commentsController = require('../controller/commentsController');
 
+router.post('/comments', commentsController.createComment);
 
+router.get('/comments/:id', commentsController.getCommentById);
+
+router.get('/posts/:postId/comments', commentsController.getCommentsByPostId);
+
+router.put('/comments/:id', commentsController.updateCommentById);
+
+router.delete('/comments/:id', commentsController.deleteCommentById);
 
 module.exports = router;
