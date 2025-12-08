baa5d429fbc196f5a39c7005feccc32ba0be7331
Tal Peled <talp2002@cs.colman.ac.il>
2025-12-08 10:05:20 +0200

    Add environment configuration and update dependencies in package.json

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
